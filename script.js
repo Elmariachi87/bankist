bf96540361d83c1b51efa1174be15a52c6935201
Joe Hookham <joehookdev@gmail.com>
2022-08-07 12:24:22 +0100

    Added cookie popup

--- script.js
+++ script.js
@@ -33,3 +33,47 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// ===== 186 - Selecting elements =====
+// document.documentElement is the real DOM element
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+// Returns a node list - will not update the node list when elements are added / removed
+const allSections = document.querySelectorAll('.section');
+
+console.log(allSections);
+
+document.getElementById('section--1');
+// For HTML tags - returns HTML collection, which will be updated when you delete / add other html elements with same tag
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+console.log('-------');
+console.log(document.getElementsByClassName('btn'));
+
+// Creating & inserting elements
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// header.prepend(message);
+
+header.append(message);
+
+// You can only have 1 version of a DOM element, so if you prepend, then append, it will overrite. So you can clone the element like this:
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });

e4c6a18fd1bc5463f9b0bc300fcbd0b77eed0000
Joe Hookham <joehookdev@gmail.com>
2022-08-12 21:48:35 +0100

    Added tabbed component

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 // Node list (similar to array) - has some methods like forEach
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -34,6 +36,93 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+// Button scrolling
+// btnScrollTo.addEventListener('click', function (e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   console.log(s1coords);
+//   console.log(e.target.getBoundingClientRect());
+//   console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+//   console.log(
+//     'height/width viewport',
+//     document.documentElement.clientHeight,
+//     document.documentElement.clientWidth
+//   );
+
+//   // Scrolling
+//   // window.scrollTo(
+//   //   s1coords.left + window.pageXOffset,
+//   //   s1coords.top + window.pageYOffset
+//   // );
+
+//   // window.scrollTo({
+//   //   left: s1coords.left + window.pageXOffset,
+//   //   top: s1coords.top + window.pageYOffset,
+//   //   behavior: 'smooth',
+//   // });
+
+//   section1.scrollIntoView({
+//     behavior: 'smooth',
+//   });
+// });
+
+// Page naviagation
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     // Stops it scrolling to the section
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// Applying this to all btns / links can create an excessive amount of functions. We can apply it to parent elements to reduce the load.
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy - this only activates if what we clicked on matches the class we want
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    console.log('LINK');
+  }
+});
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// This creates too many copies of the same function
+// tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
+
+// Use event delegation:
+// Use .closest method so that whether we click on the text (span) or the button, JS always just looks for the closest button
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause - if what we have clicked on isn't 'clicked', finish the function
+  if (!clicked) return;
+
+  // Remove active class
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+  // Active content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+  // NOTE: dataset corresponds to the html attribute data-xxx with xxx coming after 'dataset'
+});
+/*
 // ===== 186 - Selecting elements =====
 // document.documentElement is the real DOM element
 console.log(document.documentElement);
@@ -77,3 +166,181 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+// ===== 187 - Styles, Attributes and Classes =====
+// Styles
+
+// See HTML header for reference
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+// We can't access this because it's not an inline style (that we set in JS)
+console.log(message.style.height);
+// This works, as we set it
+console.log(message.style.backgroundColor);
+// We can access it this way
+console.log(getComputedStyle(message).color);
+
+// First we need to parse the value for a number (as it is in pixels)
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+// We can change CSS custom properties like this
+// Root
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes (src, alt, class, id etc.)
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src); // http://127.0.0.1:5500/img/logo.png -absolute filepath
+console.log(logo.className);
+
+// Changing the attribute
+logo.alt = 'Beautiful, minimalist logo';
+// Won't work because this attribute isn't expected in an img class
+console.log(logo.designer);
+// Can do it this way
+console.log(logo.getAttribute('designer'));
+// Set attribute - define the attribute, then the data
+logo.setAttribute('company', 'Bankist');
+
+logo.getAttribute('src');
+console.log(logo.getAttribute('src')); // img/logo.png - relative filepath
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href); // http://127.0.0.1:5500/index.html#
+console.log(link.getAttribute('href')); // #
+
+// Data attributes - special attributes that start with the word 'data'
+// Use camelCase here, wheras in html it's dash
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j', 'o');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+// Don't use - it will overrite exising classes, and it only lets you add 1 class
+logo.className = 'jonas';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+// ===== 189 - Types of events and event handlers =====
+const h1 = document.querySelector('h1');
+
+// Remove event listeners at the same time
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+  // h1.removeEventListener('mouseenter', alertH1);
+};
+
+// triggers when the mouse enters an area
+h1.addEventListener('mouseenter', alertH1);
+
+// Older way of listening for events:
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// ===== 191 - Event propagation
+
+// rgb(255, 255, 255);
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+
+  // Stop propagation - stops propagation through parent elements
+  // e.stopPropagation();
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  },
+  true // Capture parameter - this means you're getting data from the capture stage (not the bubbling), as the instructions pass through the DOM - so e.target will show NAV first because that's the highest parent element
+);
+
+
+// ===== 192 - Event delegation =====
+// See page navigation in app section
+
+// ===== 193 - Traversing the DOM
+
+const h1 = document.querySelector('h1');
+
+// Going down: children
+
+console.log(h1.querySelectorAll('.highlight')); // NodeList(2) [span.highlight, span.highlight]
+console.log(h1.childNodes); // NodeList(9) [text, comment, text, span.highlight, text, br, text, span.highlight, text]
+// For DIRECT children
+console.log(h1.children); // HTMLCollection(3) [span.highlight, br, span.highlight]
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'darkgreen';
+
+// Going upwards: parents
+
+console.log(h1.parentNode);
+
+// Closest elements
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+// If you pass an argument in which matches the initial element, it will select itself
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+// Nodes
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
+
+*/
